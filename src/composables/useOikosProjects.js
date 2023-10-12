2c7a188806b9a94831633aad659407a9993d8fdb
Shan Torrejos <117603433+shantorrejos@users.noreply.github.com>
2023-10-12 19:03:04 +0800

    Implemented fisher-yates algorithm
    To get unique tags for each project without repetition
     (just for mock data)

--- src/composables/useOikosProjects.js
+++ src/composables/useOikosProjects.js
@@ -29,15 +29,31 @@ const projects = [];
 const { users } = useOikosUsers();
 const { articles } = useOikosArticles();
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 // for projects
 for (let i = 0; i < 10; i++) {
+  const availableTags = [
+    "Agriculture",
+    "Education",
+    "Healthcare",
+    "Rural",
+    "Technology",
+    "Transportation",
+  ];
+
+  shuffleArray(availableTags);
+  const numTags = faker.number.int({ min: 1, max: 4 });
   projects[i] = {
     name: faker.lorem.words(2),
     photo: faker.image.urlPicsumPhotos(),
     description: faker.lorem.sentences(2),
-    tags: Array(faker.number.int({ min: 2, max: 4 }))
-      .fill()
-      .map(() => faker.helpers.arrayElement(["Agriculture", "Education"])),
+    tags: availableTags.slice(0, numTags),
     documentation: { photos: [], videos: [] },
     articles: Array(5)
       .fill()
